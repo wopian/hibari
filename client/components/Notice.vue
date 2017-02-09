12248df7c03f8e34ccaee362b37c9f2ed314c063
James Harris <wopiancitizen123@gmail.com>
2017-02-09 19:33:06 +0000

    Various updates

--- client/components/Notice.vue
+++ client/components/Notice.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class='alert alert-info' role='alert'>
-    <div class='container'>
+  <section role='alert'>
+    <div>
     <!--
     <h4 class='alert-heading'>Notice</h4>
     -->
       <p>As of 8th February '17, Hibari has been rewritten from scratch to support the Kitsu API</p>
-      <p class='mb-0'>This version is very early alpha and is prone to bugs and breakages. Feel free to send me suggestions on <a href='//kitsu.io/users/wopian'>Kitsu</a></p>
+      <p>This version is very early alpha and is prone to bugs and breakages. Feel free to send me suggestions on <a href='//kitsu.io/users/wopian'>Kitsu</a></p>
     </div>
-  </div>
+  </section>
 </template>
 
-<style lang='scss'>
-  .alert {
+<style lang='scss' scoped>
+  @import '~bootstrap/scss/bootstrap';
+
+  section {
+    @extend .alert;
+    @extend .alert-info;
     border-radius: 0;
     margin-bottom: 0;
+
+    div {
+      @extend .container;
+    }
+
+    P:last-of-type {
+      @extend .mb-0;
+    }
   }
 </style>
