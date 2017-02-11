f26eafd19682853288fc1f72d2793b615a1b421d
James Harris <wopiancitizen123@gmail.com>
2017-02-11 09:54:25 +0000

    Show user cover

--- client/views/User.vue
+++ client/views/User.vue
@@ -1,44 +1,52 @@
 <template>
   <main class='user'>
-    <h1>User</h1>
-
     <spinner v-if='loading'></spinner>
 
-    <div class='error' v-if='error'>
+    <section v-if='error' error>
       {{ error }}
-    </div>
+    </section>
 
-    <div class='content' v-if='user'>
-      <set-title :title='user.attributes.name + " - Hibari"'></set-title>
+    <section content>
+      <set-title v-if='user' :title='user.attr.name + " - Hibari"'></set-title>
+
+      <div class='cover' v-if='user' :style='{ backgroundImage: "url(" + user.attr.coverImage.original + ")" }'>
+        <big>Cover</big>
+      </div>
+      <div class='cover 2' v-else>
+        <big>Loading</big>
+      </div>
+
       <p>{{ $route.params.id }}</p>
-      <pre>
+      <pre v-if='user'>
 ID: {{ user.id }}
-Name: {{ user.attributes.name }}
-Past Names: {{ user.attributes.pastNames }}
-<!--About: {{ user.attributes.about }}-->
-Location: {{ user.attributes.location }}
-Waifu? {{ user.attributes.waifuOrHusbando }}
-Followers: {{ user.attributes.followersCount }}
-Following: {{ user.attributes.followingCount }}
-Joined: {{ user.attributes.createdAt }}
-Facebook: https://facebook.com/{{ user.attributes.facebookId }}
-Life Spent on Anime: {{ user.attributes.lifeSpentOnAnime }}
-Birthday: {{ user.attributes.birthday }}
-Gender: {{ user.attributes.gender }}
-Last Updated: {{ user.attributes.updatedAt }}
-Favourites: {{ user.attributes.favouritesCount }}
-Ratings: {{ user.attributes.ratingsCount }}
-Posts: {{ user.attributes.postsCount }}
-Comments: {{ user.attributes.commentsCount }}
-Likes Given: {{ user.attributes.likesGivenCount }}
-Likes Received: {{ user.attributes.likesReceivedCount }}
-Reviews: {{ user.attributes.reviewsCount }}
-Pro: {{ user.attributes.proExpiresAt }}
+Name: {{ user.attr.name }}
+Past Names: {{ user.attr.pastNames }}
+<!--About: {{ user.attr.about }}-->
+Location: {{ user.attr.location }}
+Waifu? {{ user.attr.waifuOrHusbando }}
+Followers: {{ user.attr.followersCount }}
+Following: {{ user.attr.followingCount }}
+Joined: {{ user.attr.createdAt }}
+Facebook: https://facebook.com/{{ user.attr.facebookId }}
+Life Spent on Anime: {{ user.attr.lifeSpentOnAnime }}
+Birthday: {{ user.attr.birthday }}
+Gender: {{ user.attr.gender }}
+Last Updated: {{ user.attr.updatedAt }}
+Favourites: {{ user.attr.favouritesCount }}
+Ratings: {{ user.attr.ratingsCount }}
+Posts: {{ user.attr.postsCount }}
+Comments: {{ user.attr.commentsCount }}
+Likes Given: {{ user.attr.likesGivenCount }}
+Likes Received: {{ user.attr.likesReceivedCount }}
+Reviews: {{ user.attr.reviewsCount }}
+Pro: {{ user.attr.proExpiresAt }}
       </pre>
 
-      <img v-bind:src='user.attributes.avatar.large'>
-      <img v-bind:src='user.attributes.coverImage.original'>
-    </div>
+      <img v-if='user' v-bind:src='user.attr.avatar.large'>
+      <img v-if='user' v-bind:src='user.attr.coverImage.original'>
+
+      <pre v-if='user'>{{ user }}</pre>
+    </section>
   </main>
 </template>
 
@@ -86,6 +94,10 @@ export default {
           this.error = 'No user exists'
         } else {
           this.user = data.body.data[0]
+          this.user.attr = this.user.attributes
+          delete this.user.attributes
+          //delete this.user.relationships
+          delete this.user.links
         }
       })
       .catch((error) => {
@@ -99,5 +111,17 @@ export default {
 <style lang='scss'>
   pre {
     font-size: 50%;
+  }
+
+  main.user {
+    section[content] {
+      .cover {
+        width: 100vw;
+        height: 400px;
+        background-color: darken(white, 15);
+        background-size: cover;
+        background-position: center;
+      }
+    }
   }
 </style>
