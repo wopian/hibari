6860ee637877289042369e7ea1b172af74f33bb4
James Harris <wopiancitizen123@gmail.com>
2017-03-20 10:03:19 +0000

    chore: use electron-screenshot-service until blink support in pageres

--- build/pageres.js
+++ build/pageres.js
@@ -1,10 +1,27 @@
+/*
+// Wait until Pageres supports headless chrome
 const PageRes = require('pageres')
 
-const pageres = new PageRes({ delay: 10 })
-  .src('hb.wopian.me', [ '412x732', '1920x1080' ])
+const pageres = new PageRes({ delay: 20 })
+  .src('hb.wopian.me/#/', [ '412x732', '1920x1080' ])
   .src('hb.wopian.me/#/@wopian', [ '412x732', '1920x1080' ])
-  .src('https://hb.wopian.me/#/anime/cowboy-bebop', [ '412x732', '1920x1080' ])
-  .src('https://hb.wopian.me/#/manga/20th-century-boys', [ '412x732', '1920x1080' ])
   .dest(`${__dirname}/../screenshot`)
   .run()
   .then(() => console.log('done'))
+*/
+
+var fs = require('fs')
+var screenshot = require('electron-screenshot-service')
+
+screenshot({
+  url : 'https://hb.wopian.me/#/@wopian',
+  width : 1920,
+  height : 1080,
+  delay: 1000 * 10,
+  css: 'body{ background:transparent !important;}\n::-webkit-scrollbar{opacity:0 !important;display: none !important;}'
+})
+.then(function(img){
+  fs.writeFile('./screenshots/user.png', img.data, function(err){
+    screenshot.close()
+  })
+})
