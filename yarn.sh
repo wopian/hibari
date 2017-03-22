ad6178fa33e2c6ea57dc15140b959eb5d45d42d6
James Harris <wopiancitizen123@gmail.com>
2017-03-22 00:57:30 +0000

    chore(travis): switch to using yarn

--- yarn.sh
+++ yarn.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-echo "Should yarn.lock be regenerated?"
 if [[ $TRAVIS_PULL_REQUEST_BRANCH != *"greenkeeper"* ]]; then
 	# Not a GreenKeeper Pull Request, aborting
 	exit 0
 fi
 
-echo "Cloning repo"
 git clone "https://"$GH_TOKEN"@github.com/"$TRAVIS_REPO_SLUG".git" repo
 cd repo
 
-echo "Switching to branch $TRAVIS_PULL_REQUEST_BRANCH"
 git checkout $TRAVIS_PULL_REQUEST_BRANCH
 
 # See if commit message includes "update"
 git log --name-status HEAD^..HEAD | grep "update" || exit 0
 
-echo "(Creat/updat)ing lockfile"
+# Run yarn to create/update lockfile
 yarn
 
-echo "Commit and push yarn.lock"
+# Commit and push yarn.lock file
 git config --global user.email "$GH_EMAIL"
 git config --global user.name "$GH_USERNAME"
 git config --global push.default simple
