7735322394802c1eca1b674754441407438dc0fd
James Harris <wopian@wopian.me>
2017-08-01 03:31:45 +0100

    Update vue-loader.conf.js

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -2,6 +2,16 @@ var utils = require('./utils')
 var config = require('../config')
 var isProduction = process.env.NODE_ENV === 'production'
 
+var AUTOPREFIXER_BROWSERS = [
+  'Android >= 57',
+  'Chrome >= 57',
+  'Firefox >= 53',
+  'Explorer >= 11',
+  'iOS >= 10',
+  'Opera >= 43',
+  'Safari >= 10',
+];
+
 module.exports = {
   loaders: utils.cssLoaders({
     sourceMap: isProduction
@@ -10,8 +20,6 @@ module.exports = {
     extract: isProduction
   }),
   postcss: [
-    require('autoprefixer')({
-      browsers: ['last 1 versions']
-    })
+    require('autoprefixer')({ browsers: AUTOPREFIXER_BROWSERS })
   ]
 }
