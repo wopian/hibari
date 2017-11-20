167209f9ae202035be07d7565c0d11ddc38f85e6
James Harris <wopian@wopian.me>
2017-11-20 20:26:35 +0000

    refactor: code split locales

--- src/locale/index.js
+++ src/locale/index.js
@@ -1,16 +1,15 @@
 import Vue from 'vue'
 import I18n from 'vue-i18n'
 import store from '../store'
-import en from './en'
-import ja from './ja'
 
 Vue.use(I18n)
 
 const locale = store.state.preferences.language || 'en'
 
 const messages = {
-  en,
-  ja
+  en: import('./en'),
+  ja: import('./ja'),
+  nl: import('./nl')
 }
 
 export default new I18n({
