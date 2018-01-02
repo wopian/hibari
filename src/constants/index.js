2f928fe34997714c6b7f6cdbdcdacf1f0c14e294
James Harris <wopian@wopian.me>
2018-01-02 03:07:25 +0000

    feat: alphabetically sort locale selection list

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,7 @@
 export const DEFAULT_LANGUAGE = 'en'
 export const FALLBACK_LANGUAGE = DEFAULT_LANGUAGE
-export const SUPPORTED_LANGUAGES = [
-  DEFAULT_LANGUAGE,
-  'ja',
-  'nl',
-  'es'
-]
+
+export const SUPPORTED_LANGUAGES = ['ja', 'nl', 'es']
+
+// Alpabetically sort with English as first option
+SUPPORTED_LANGUAGES.sort((a, b) => a.localeCompare(b)).unshift(DEFAULT_LANGUAGE)
