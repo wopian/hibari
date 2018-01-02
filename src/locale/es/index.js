617b6e375d0e1b80ca17399a26c5c0f6a2047e65
Juan José Salvador Piedra <juanjosalvador@netc.eu>
2018-01-02 02:27:20 +0100

    Create index.js

---
+++ src/locale/es/index.js
@@ -0,0 +1,38 @@
+export default {
+  lang: 'Español',
+  hibari: 'Hibari',
+  kitsu: 'Kitsu',
+  media: {
+    anime: 'Anime',
+    drama: 'Drama',
+    manga: 'Manga'
+  },
+  login: {
+    login: 'Entrar',
+    emailLabel: 'Email / Nombre de usuario',
+    emailPlaceholder: 'Tu email o nombre de usuario',
+    passwordLabel: 'Contraseña',
+    passwordPlaceholder: 'Tu contraseña',
+    loginWithKitsu: 'Entrar con @:kitsu'
+  },
+  loggedIn: {
+    profile: 'Perfil',
+    preferences: 'Preferencias',
+    logout: 'Salir'
+  },
+  navigation: {
+    library: 'Biblioteca',
+    anime: '@:media.anime',
+    manga: '@:media.manga',
+    bugs: 'Bugs y características'
+  },
+  footer: {
+    builtBy: '{0} por {1}',
+    sourceCode: 'Código fuente licenciado bajo {0}',
+    contentProvidedBy: 'Todo el contenido ha sido provisto por {0}'
+  },
+  landing: {
+    title: 'Seguir, compartir y descubrir {0}',
+    subtitle: 'Un cliente Open Source Vue para Kitsu.io'
+  }
+}
